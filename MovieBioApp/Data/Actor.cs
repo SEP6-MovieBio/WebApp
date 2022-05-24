78eeafa607c73f690485a51a3fb8fd54af8639a9
Kevin <kevin.studie@outlook.dk>
2022-05-24 12:43:26 +0200

    Top 200 sites

---
+++ MovieBioApp/Data/Actor.cs
@@ -0,0 +1,22 @@
+﻿namespace MovieBioApp.Data
+{
+    public class Actor
+    {
+        public  string FirstName { get; set; }
+        public  string LastName { get; set; }
+        public  double Rating { get; set; }
+        public  int Age { get; set; }
+        public  string TopMovieStarredIn { get; set; }
+        public string Picture { get; set; }
+    
+        public Actor(string firstName, string lastName, double rating, int age, string topMovieStarredIn, string picture)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Rating = rating;
+            Age = age;
+            TopMovieStarredIn = topMovieStarredIn;
+            Picture = picture;
+        }
+    }
+}
